8664e29e087c2abc2d135936ec8a5169b910af8a
arthur <yahui9119@163.com>
2018-05-14 16:22:50 +0800

    ignoreWarnings

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry ,YellowBox} from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 import App from './src';
-
 AppRegistry.registerComponent('DvaTS', () => App);
